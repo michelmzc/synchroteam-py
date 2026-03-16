05c94444c9c1f3915401c45b8d52b284e5f6a22a
Michel <michelmzco@gmail.com>
2026-03-16 16:49:16 -0300

    updating example

--- example.py
+++ example.py
@@ -1,9 +1,17 @@
-# Configure you enviroment variables or .env
+# First configure you enviroment variables or .env file and activate
 
 from synchroteam_py import SynchroteamClient
 
 client = SynchroteamClient()
 
+# Test API connetction
+try:
+    print(client.test_connection())
+    print("Connected")
+except Exception as error:
+    print("Error in connection", e)
+
+# Test of jobs endpoint
 jobs_types = client.jobs.get_job_types()
 
 print(jobs_types)
